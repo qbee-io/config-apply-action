91902d7af3800b38722cf936da7265f0d2b96b8a
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2024-06-13 14:39:24 +0200

    Convert to qbee-cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,21 @@
-#!/bin/bash -l
+#!/bin/sh
 
 #inputs
-token=$1
-file_path=$2
-node_id=$3
-form_type=$4
-commit_message=$5
-api_host=$6
+config_path=$1
+entity_id=$2
+bundle=$3
+commit_message=$4
+template_params=$5
+is_tag=$6
 
-successful_status_code='200'
-echo -e "init token: $token\nfilename: $file_path\nnode_id: $node_id\nform_type: $form_type\ncommit_message: $commit_message\napi_host: $api_host"
+TARGET_OPT="--node $entity_id"
 
-config_payload=$(jq -rc --arg node_id "$node_id" --arg formtype "$form_type" '{"node_id":$node_id,"formtype":$formtype,"config":.}' < "$file_path")
-config_status=$(curl -s -o /dev/null -X POST --header "Authorization: Bearer $token" \
-  -H "Content-type: application/json" --url "https://${api_host}/api/v2/change" \
-  -d "${config_payload}" -w "%{http_code}")
-
-if [[ "$config_status" != "$successful_status_code" ]]; then
-    echo "http_code was - $config_status"
-    echo "something went wrong ... aborting"
-    exit 1
-else
-    echo "http_code was - $config_status"
+if [[ -n $is_tag ]]; then
+  TARGET_OPT="--tag $entity_id"
 fi
 
-commit_payload=$(jq -nrc --arg commit_message "$commit_message" '{"action":"commit","message":$commit_message}')
-commit_status=$(curl -s -o /dev/null -X POST --header "Authorization: Bearer $token" \
-  -H "Content-type: application/json" --url "https://${api_host}/api/v2/commit" \
-  -d "${commit_payload}" -w "%{http_code}")
+qbee-cli config save --config $config_path --bundle $bundle $TARGET_OPT
 
-if [[ "$commit_status" != "$successful_status_code" ]]; then
-    echo "http_code was - $commit_status"
-    echo "something went wrong ... aborting"
-    exit 1
-else
-    echo "http_code was - $commit_status"
+if [[ -n "$commit_message" ]]; then
+  qbee-cli config commit --commit-message "$commit_message"
 fi
